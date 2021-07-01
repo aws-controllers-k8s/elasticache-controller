190dcaeaf0572f5d8a1c10e07bb460432f9a4c0c
Eric Chen <ercche@amazon.com>
2021-06-30 17:11:58 -0700

    regenerate code

--- pkg/resource/snapshot/sdk.go
+++ pkg/resource/snapshot/sdk.go
@@ -599,6 +599,7 @@ func (rm *resourceManager) setStatusDefaults(
 // else it returns nil, false
 func (rm *resourceManager) updateConditions(
 	r *resource,
+	onSuccess bool,
 	err error,
 ) (*resource, bool) {
 	ko := r.ko.DeepCopy()
@@ -607,12 +608,16 @@ func (rm *resourceManager) updateConditions(
 	// Terminal condition
 	var terminalCondition *ackv1alpha1.Condition = nil
 	var recoverableCondition *ackv1alpha1.Condition = nil
+	var syncCondition *ackv1alpha1.Condition = nil
 	for _, condition := range ko.Status.Conditions {
 		if condition.Type == ackv1alpha1.ConditionTypeTerminal {
 			terminalCondition = condition
 		}
 		if condition.Type == ackv1alpha1.ConditionTypeRecoverable {
 			recoverableCondition = condition
+		}
+		if condition.Type == ackv1alpha1.ConditionTypeResourceSynced {
+			syncCondition = condition
 		}
 	}
 
@@ -654,9 +659,11 @@ func (rm *resourceManager) updateConditions(
 			recoverableCondition.Message = nil
 		}
 	}
+	// Required to avoid the "declared but not used" error in the default case
+	_ = syncCondition
 	// custom update conditions
 	customUpdate := rm.CustomUpdateConditions(ko, r, err)
-	if terminalCondition != nil || recoverableCondition != nil || customUpdate {
+	if terminalCondition != nil || recoverableCondition != nil || syncCondition != nil || customUpdate {
 		return &resource{ko}, true // updated
 	}
 	return nil, false // not updated
