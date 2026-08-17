7bf23dccad7b699a5081064f08d26752db2daf23
knottnt <trevor.knottnerus@gmail.com>
2026-08-17 14:28:36 -0700

    Re-generate with updated code-generator

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,6 +27,6 @@ var (
 // that generated this controller's code. They are baked in at code-generation
 // time and are immutable for the life of the generated code.
 const (
-	ACKGenerateVersion   = "v0.62.1"
-	ACKGenerateBuildDate = "2026-08-13T00:57:17Z"
+	ACKGenerateVersion   = "v0.62.1-4-gd4aaca5"
+	ACKGenerateBuildDate = "2026-08-17T21:26:28Z"
 )
